0fe2041fb1de208e509de92f9d3775b62ebccbdb
Sanil-55 <43237952+Sanil-55@users.noreply.github.com>
2018-11-09 18:49:09 +0530

    Create mario.c

---
+++ sanil/pset1/mario.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main()
+{
+    int height;
+    do
+    {
+        printf("height: ");
+        height = GetInt();
+    } 
+    while (height < 0 || height > 23);
+    
+    // Generate and draw the half-pyramid
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < height - i - 1; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k <= i + 1; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
